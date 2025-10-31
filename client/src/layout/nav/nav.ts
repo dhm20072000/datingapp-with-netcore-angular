f611ac0f0455afca8a1ab801cfc28e78d1496770
Minh Dang <minh.dang@omega365.com>
2025-10-31 21:38:51 +0700

    Updating resources

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -4,6 +4,7 @@ import { AccountService } from '../../core/services/account-service';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { ToastService } from '../../core/services/toast-service';
 import { themes } from '../theme';
+import { BusyService } from '../../core/services/busy-service';
 
 @Component({
   selector: 'app-nav',
@@ -13,6 +14,7 @@ import { themes } from '../theme';
 })
 export class Nav implements OnInit {
   protected accountService = inject(AccountService);
+  protected busyService = inject(BusyService);
   private router = inject(Router);
   private toast = inject(ToastService);
   protected creds: any = {};
@@ -29,7 +31,7 @@ export class Nav implements OnInit {
     document.documentElement.setAttribute('data-theme', theme);
     const elem = document.activeElement as HTMLDivElement;
     if(elem) elem.blur();
-  }
+  };
   
   login(){
     this.accountService.login(this.creds).subscribe({
